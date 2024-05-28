2e3f2874cfc763929ee8d2a494ce5ace83cdd56d
Stephen Angelo <105164926+Steve0a0@users.noreply.github.com>
2024-05-28 15:52:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,14 @@ import Service from "./Components/Service";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import './index.css';
-import { useState, useEffect } from "react";
-// import ScaleLoader from "react-spinners/ScaleLoader"; // Import ScaleLoader
+
+
 
 function App() {
-  // const [loading, setLoading] = useState(true); // Set loading initially to true
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false); // Set loading to false after 5 seconds
-  //   }, 3000);
-  // }, []);
-
-  // const color = "#ffcc5c"; // Define your desired color here
+  
 
   return (
     <div className="App">
-      {/* {loading ? (
-        <div className="loading-spinner">
-          <ScaleLoader color={color} loading={loading} size={40} />
-        </div>
-      ) : (
-        <> */}
           <Navbar />
           <Hero />
           <Carousel />
@@ -34,8 +21,7 @@ function App() {
           <About />
           <Contact />
           <Footer />
-        {/* </>
-      )} */}
+        
     </div>
   );
 }
