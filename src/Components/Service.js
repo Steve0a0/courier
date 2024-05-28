a2ab648cf5c23041af0301995e2fff709166d10b
Stephen Angelo <105164926+Steve0a0@users.noreply.github.com>
2024-05-28 15:31:59 +0100

    Update Service.js

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -4,25 +4,25 @@ import { faTruck, faDolly, faPlaneDeparture } from '@fortawesome/free-solid-svg-
 import './Service.css';
 
 const Service = () => {
-  window.addEventListener('scroll', reveal);
+  // window.addEventListener('scroll', reveal);
 
-  function reveal(){
-    var reveals = document.querySelectorAll('.reveal')
+  // function reveal(){
+  //   var reveals = document.querySelectorAll('.reveal')
 
-    for (var i=0;i<reveals.length;i++){
-      var windowheight = window.innerHeight;
-      var revealtop = reveals[i].getBoundingClientRect().top;
-      var revealpoint = 150;
+  //   for (var i=0;i<reveals.length;i++){
+  //     var windowheight = window.innerHeight;
+  //     var revealtop = reveals[i].getBoundingClientRect().top;
+  //     var revealpoint = 150;
 
-      if (revealtop < windowheight -revealpoint){
-        reveals[i].classList.add('active');
-      }
-      else{
-        reveals[i].classList.remove('active');
+  //     if (revealtop < windowheight -revealpoint){
+  //       reveals[i].classList.add('active');
+  //     }
+  //     else{
+  //       reveals[i].classList.remove('active');
 
-      }
-    }
-  }
+  //     }
+  //   }
+  // }
   return (
     <div className="containers">
       <h1>Our Services</h1>
